e0d1f0b17f039722316b91ab5cde6899f6b3adda
Lee Fitchett <lefitche@microsoft.com>
2019-09-16 12:06:11 -0700

    remove using

--- modules/DeviceDiagnostics/Program.cs
+++ modules/DeviceDiagnostics/Program.cs
@@ -7,7 +7,6 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using DeviceDiagnostics;
 using DeviceStreamsUtilities;
 using Microsoft.Azure.Devices.Client;
 
