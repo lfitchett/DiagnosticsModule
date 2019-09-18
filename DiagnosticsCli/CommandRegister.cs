786b43eca7a795baea38c4e8dea920caaa423def
Lee Fitchett <lefitche@microsoft.com>
2019-09-18 08:06:20 -0700

    move command registry to own file

---
+++ DiagnosticsCli/CommandRegister.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+
+namespace DiagnosticsCli
+{
+    public static class CommandRegister
+    {
+        /// <summary>
+        ///     Registers all commands
+        ///         getfile -s <targetFileName> -d <destinationFileName> : sends file from module to cli device
+        ///         ls : lista files in module shared folder
+        /// </summary>
+        /// <param name="manager"></param>
+        /// <param name="parser"></param>
+        public static void RegisterCommands(HttpClient httpClient, CommandLineParser parser, CancellationToken ct)
+        {
+            /* Register list file command */
+            parser.RegisterCommand("ls", async () =>
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(@"http://localhost:80/api/file/list", ct);
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine(await response.Content.ReadAsStringAsync());
+                }
+            });
+
+            /* Register get file */
+            string source = null;
+            Action<string> setSource = (string val) => source = val;
+
+            string destination = null;
+            Action<string> setDest = (string val) => destination = val;
+
+            Dictionary<string, Action<string>> args = new Dictionary<string, Action<string>>
+                        {
+                            {"--source", setSource },
+                            {"-s", setSource },
+                            {"--destination", setDest },
+                            {"-d", setDest }
+                        };
+
+            parser.RegisterCommand("getfile", async () =>
+            {
+                if (source == null)
+                {
+                    Console.WriteLine("Please use -s to set a source");
+                    return;
+                }
+                if (destination == null)
+                {
+                    Console.WriteLine("Please use -d to set a destination");
+                    return;
+                }
+
+                UriBuilder uri = new UriBuilder(@"http://localhost:80/api/file");
+                uri.Query = $"filename=" + Uri.EscapeUriString(source);
+                HttpResponseMessage response = await httpClient.GetAsync(uri.Uri, ct);
+
+                Console.WriteLine(response);
+                if (response.IsSuccessStatusCode)
+                {
+                    using (FileStream file = File.OpenWrite($@"C:\Users\Lee\Documents\Test\To\{destination}"))
+                    {
+                        (await response.Content.ReadAsStreamAsync()).CopyTo(file);
+                    }
+                }
+            }, args);
+        }
+    }
+}
--- DiagnosticsCli/Program.cs
+++ DiagnosticsCli/Program.cs
@@ -37,10 +37,9 @@ namespace DiagnosticsCli
             using (ClientWebSocket webSocket = await client.ConnectToDevice(deviceId, ct))
             using (HttpClient httpClient = new HttpClient(new WebsocketHttpMessageHandler(webSocket)))
             {
-                /* TODO: find open source version. This is intended as a temporary hack. there should be a library that does this. */
+                /* TODO: find open source version. This is intended to be temporary. There should be a library that does this. */
                 CommandLineParser parser = new CommandLineParser();
-
-                RegisterCommands(httpClient, parser, ct);
+                CommandRegister.RegisterCommands(httpClient, parser, ct);
 
                 while (true)
                 {
@@ -56,68 +55,6 @@ namespace DiagnosticsCli
 
                 await webSocket.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "Normal close", CancellationToken.None);
             }
-        }
-
-        /// <summary>
-        ///     Registers the "getfile" command. when getfile is used, it will parse source and destination and ask the module for the given file.
-        /// </summary>
-        /// <param name="manager"></param>
-        /// <param name="parser"></param>
-        static void RegisterCommands(HttpClient httpClient, CommandLineParser parser, CancellationToken ct)
-        {
-            /* Register list file command */
-            parser.RegisterCommand("ls", async () =>
-            {
-                HttpResponseMessage response = await httpClient.GetAsync(@"http://localhost:80/api/file/list", ct);
-                if (response.IsSuccessStatusCode)
-                {
-                    Console.WriteLine(await response.Content.ReadAsStringAsync());
-                }
-            });
-
-            /* Register get file */
-            string source = null;
-            Action<string> setSource = (string val) => source = val;
-
-            string destination = null;
-            Action<string> setDest = (string val) => destination = val;
-
-            Dictionary<string, Action<string>> args = new Dictionary<string, Action<string>>
-                        {
-                            {"--source", setSource },
-                            {"-s", setSource },
-                            {"--destination", setDest },
-                            {"-d", setDest }
-                        };
-
-            //source = "New Text Document.txt";
-            //destination = @"C:\Users\Lee\Documents\Test\To\testFile.txt";
-            parser.RegisterCommand("getfile", async () =>
-            {
-                if (source == null)
-                {
-                    Console.WriteLine("Please use -s to set a source");
-                    return;
-                }
-                if (destination == null)
-                {
-                    Console.WriteLine("Please use -d to set a destination");
-                    return;
-                }
-
-                UriBuilder uri = new UriBuilder(@"http://localhost:80/api/file");
-                uri.Query = Uri.EscapeUriString(source);
-                HttpResponseMessage response = await httpClient.GetAsync(uri.Uri, ct);
-
-                Console.WriteLine(response);
-                if (response.IsSuccessStatusCode)
-                {
-                    using (FileStream file = File.OpenWrite($@"C:\Users\Lee\Documents\Test\To\{destination}"))
-                    {
-                        (await response.Content.ReadAsStreamAsync()).CopyTo(file);
-                    }
-                }
-            }, args);
-        }
+        }        
     }
 }
